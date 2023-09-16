2d1468e97b8345d45c834c2811c622589028e5ec
Yohan Capezzz <yohan.capezza@epitech.eu>
2023-09-16 16:24:50 +0200

    Amélioration encryption_task2

--- encryption_task2.py
+++ encryption_task2.py
@@ -2,12 +2,12 @@ message2 = input("Entrer une chaine de caractère : ")
 cle = "cle"
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 code = ""
+iCLE = 0
 for i in range(0, len(message2)):
     letter2 = message2[i]
-    if i >= len(cle):
-        letterCLE = cle[i - len(cle)]
-    else:
-        letterCLE = cle[i]
+    if iCLE >= len(cle):
+        iCLE = 0
+    letterCLE = cle[iCLE]
     res = alphabet.rfind(letter2) + alphabet.rfind(letterCLE)
     code += alphabet[res]
 #    print(res)
@@ -17,10 +17,9 @@ print(code)
 newMessage = ""
 for i2 in range(0, len(code)):
     letter2 = code[i2]
-    if i2 >= len(cle):
-        letterCLE = cle[i2 - len(cle)]
-    else:
-        letterCLE = cle[i2]
+    if iCLE >= len(cle):
+        iCLE = 0
+    letterCLE = cle[iCLE]
     res = alphabet.rfind(letter2) - alphabet.rfind(letterCLE)
     if res < 0:
         res += 26
